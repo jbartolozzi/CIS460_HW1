40bcc55c45c1e52e98da3a56aace3e73e152e7f7
James Bartolozzi <bartolozzij@gmail.com>
2013-09-17 14:39:38 -0400

    added raymarch

--- CIS460_Homework1/raymarch.cpp
+++ CIS460_Homework1/raymarch.cpp
@@ -10,3 +10,20 @@ raymarch::raymarch() {
 	cam->printImage(outname);
 	cout << "Successfully output file named: " << outname << endl;
 }
+
+// Ray = Eye + t (P-E)/|P-E|  glm::normalize(getDirectionFromCoordinate(i,j)-eye))
+// r = xe + n*s
+
+void raymarch::calculateValues() {
+	float step = fr->STEP;
+	for (int sx = 0; sx < fr->RESO.x; sx++) {
+		for(int sy = 0; sy < fr->RESO.y; sy++) {
+			glm::vec3 outColor(0,0,0);
+			for(float t; t < 100; t+=step) {
+				glm::vec3 ray = cam->eye + glm::normalize(cam->getDirectionFromCoordinate(sx,sy)) * t;
+				glm::vec3 G = fr->MRGB * (vb->get(ray)).density;
+
+			}
+		}
+	}
+}
--- CIS460_Homework1/raymarch.h
+++ CIS460_Homework1/raymarch.h
@@ -11,6 +11,7 @@ public:
 	Camera* cam;
 	voxelBuffer* vb;
 	raymarch();
+	void calculateValues();
 };
 
 #endif
